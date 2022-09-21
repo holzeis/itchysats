fabd3c34e24df1798d8f719f3c2ea6bf26bb660c
Philipp Hoenisch <philipp@coblox.tech>
2022-09-21 13:55:54 +1000

    chore(electron): extract config into separate file
    Signed-off-by: Philipp Hoenisch <philipp@coblox.tech>

---
+++ taker-electron/forge.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "packagerConfig": {
+        "name": "ItchySats",
+        "appBundleId": "ItchySats",
+        "icon": "logo.icns",
+        "overwrite": true,
+        "appVersion": "0.6.1",
+    },
+    "makers": [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "taker_electron",
+            },
+        },
+        {
+            "name": "@electron-forge/maker-zip",
+            "platforms": [
+                "darwin",
+            ],
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {},
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {},
+        },
+    ],
+};
